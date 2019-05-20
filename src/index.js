63870fc8413e43a57f5375ba72770e9179a0ac27
Aleksandra Jaroszewska <aleksandra.jaroszewska13@gmail.com>
2019-05-20 19:19:54 +0200

    canvas
    canvas basic

--- src/index.js
+++ src/index.js
@@ -4,10 +4,26 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 class CanvasExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.canvas = React.createRef;
+  }
+  draw() {
+    const ctx = this.canvas.current.getContext("2d");
+    const size = this.props;
+    ctx.clearRect(0, 0, size, size);
+    ctx.fillStyle = "rgba(0, 150, 0, 0.3)";
+    ctx.fillRect(30, 30, 150, 150);
+  }
+  componentDidMount() {
+    this.draw();
+  }
   render() {
     const size = this.props.size;
+
     return (
       <canvas
+        ref={this.canvas}
         style={{ border: "5px solid deeppink" }}
         width={size}
         height={size}
